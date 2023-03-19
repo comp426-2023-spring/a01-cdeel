24aa318ca8bd31e9ab7192296819c0dac382807e
cdeel <carson.deel@gmail.com>
2023-03-19 15:45:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
-const port = args["port"] || 3000;
+const port = args.port || 3000;
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
@@ -18,8 +18,8 @@ fs.readFile('./public/index.html', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 if(err) {
-    throw err;
-    console.log(data);
+    console.error(err);
+    return;
 }
 
 
@@ -31,7 +31,7 @@ if(err) {
 // 1. status code 200, 
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
-const server = http.createServer(req, res => {
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end(data);
